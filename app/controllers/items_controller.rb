7a8b12f04e894f81c28950c5ffbb360e669cb720
noche6 <104665556+noche6@users.noreply.github.com>
2022-05-24 15:00:53 +0900

    ログイン、アウトでの出品画面へんか

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except:[:index]
   def index
     @items = Item.all
   end
@@ -7,4 +7,19 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+
+  def create
+    @items = Item.new( items_params )
+    if  @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:price, :item_name, :description, :image, :category_id,:situation_id,:burden_id,:prefectures_id,:delivery_id).merge(user_id: current_user.id)
+  end
+
 end
